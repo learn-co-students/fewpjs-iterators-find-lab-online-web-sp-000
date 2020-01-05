2f3b73ea8437e060ace2f0907af1640f29fb8391
stabassum <sumu1993@gmail.com>
2020-01-05 17:16:16 -0500

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const testVar = {}
 
 function testFunc() {
   return "hi"
+}
 
-  for(){
-    
-  }
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
