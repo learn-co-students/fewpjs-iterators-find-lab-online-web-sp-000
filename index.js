122a501b3fb162db29433847a9473ada7e9316f6
ericabasak <ericabasak@gmail.com>
2020-01-05 16:45:39 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin(record) {
+//   let result = record.find(record => {
+//       if (record.result === "W"){
+//         return record.year;
+//       } else {
+//         return "1969";
+//       }
+//   })
+// }
+
+function superbowlWin(record) {
+  let result = record.find(e => e.result === "W")
+  return result ? result.year : undefined
+}
