25d28ff9262a9f1cef2b1f61570a0d15f0097942
Paul Rosero <paul_rosero@yahoo.com>
2020-02-23 20:37:44 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let answer = array.find(result => result.result === "W")
+  if (answer === undefined) { 
+    return answer
+  } else {
+    return answer.year
+  }
+}
