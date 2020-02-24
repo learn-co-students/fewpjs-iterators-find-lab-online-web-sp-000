84da907eabc7b2a0fcc801a82ed940fd11fe9d65
Michael <kofronmichael@gmail.com>
2020-02-23 19:00:09 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let final = array.find(function(object){
+    return object.result === "W"
+  })
+
+  if (final){
+    return final.year
+  } else {
+    return undefined
+  }
+}
