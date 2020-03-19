c92dad7b0e055c8749b31edc569f73c97d249dad
Amy Westcott <amy.westcott@yahoo.com>
2020-03-18 19:17:43 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  const result = record.find(record => record.result === "W")
+  if (result) {
+    return result.year  
+  } else {
+    return result
+  }
+}
