e6bc9a9691bce404af67ad57cd5a4fb1d7491e1b
BlythePollard <github email address>
2020-03-25 18:51:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  const game = array.find( ({result}) => result === 'W')
+  if(game) {return game.year}
+  //use find to see if result is "w"
+  //function returns year the win ocurred
 }
