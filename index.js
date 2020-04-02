772f3040d6c4e18185d7c154977570985b91efd1
Eric Clark <eclark0013@gmail.com>
2020-04-02 15:40:06 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const result = array.find(obj => obj["result"] === "W")
+  if (result){
+    return result["year"]
+  }
+  else {
+    return undefined
+  }
+}
