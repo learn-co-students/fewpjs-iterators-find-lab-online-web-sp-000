519d6ba000713f2da97059ef5ac0d380eb6ea9b7
Lauren McCoy <lm_lutec@outlook.com>
2020-04-05 15:32:12 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let t = arr.find(team => team.result === "W" )
+ 
+  if (!!t){
+    return t.year
+  }
+  else {
+    return undefined
+  }
+}
+
+
