c5262d94069ee243020554c2782ec1504da758f0
Soner Mezgitci <mezgitci9@gmail.com>
2020-04-14 23:59:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import { getHashes } from "crypto"
+
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+//  rec = [  .., { year: "1970", result: "N/A"},   { year: "1969", result: "W"} , ... ]
+// array of getHashes
+//s = { year: "1970", result: "N/A"}
+
+function superbowlWin(rec) {
+  let entry = rec.find ( s => s['result'] === "W" );
+  return (typeof entry === "object")? entry['year']: undefined
 }
