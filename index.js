e1630ac564b5112f48e3a4458fc365e5bea03690
juan siu <juan.siu@gmail.com>
2020-06-12 14:48:02 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayObjects){
+    for(let i = 0; i < arrayObjects.length; i++){
+      if (Object.values(arrayObjects[i]).find(a => a === "W" )){
+      return arrayObjects[i]["year"];
+      };
+    }
+  }
