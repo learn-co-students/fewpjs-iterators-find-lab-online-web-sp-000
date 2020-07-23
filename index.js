74de8eae93ceaf1b0941cdc3a1f2b7d9c3ac248d
SPerfors <stevecanbuild@gmail.com>
2020-07-23 15:11:04 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(srcArray) {
+  let winner = srcArray.find(loser => loser.result === 'W' );
+  if (!!winner) {
+    return winner.year
+  }
+}
