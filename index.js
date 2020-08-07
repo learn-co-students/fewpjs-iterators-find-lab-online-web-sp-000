ca432964bea1da66986928fd037bbb3f14458c42
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-08-08 00:38:25 +0200

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let winRecord = arr.find(r => r.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
