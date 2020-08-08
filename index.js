2faf7d45462fd3ea5952c4f6326c07ad55270142
Yahyagok <ygok17@hotmail.com>
2020-08-08 07:00:29 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+   let ray = array.find(a => a.result === "W")
+   return !!ray ? ray.year : undefined 
+}
