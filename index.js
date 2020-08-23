640f034c2c8c5e63531e92079c0e73b1136afc04
decentralvision <36858744+decentralvision@users.noreply.github.com>
2020-08-22 23:30:16 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  if (record.find(r => r.result == "W" )) {
+    return record.find(r => r.result == "W" ).year
+  } else {
+    return undefined
+  }
+  
+} 
+
