bef33e91095c5a1c9358b6a01e0e2ce07d452395
Cole Rau <colevrau@gmail.com>
2020-09-03 17:17:18 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let test;
+  test = array.find( obj => obj.result === "W");
+  if (test) {
+    return test.year;
+  } else {
+    return undefined;
+  }
+}
