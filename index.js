f08c2f5b5b4c5f34854dcb6c88181f440597099c
KBENBENECK <kbenbeneck@gmail.com>
2020-09-30 23:47:51 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+// arr.find(callback(element[, index[, array]])[, thisArg]) 
+
+function superbowlWin(arr) {
+  let result = arr.find( e => e.result === 'W' ); 
+  return  !!result ? result.year : undefined
+  
+}
