e71cd15320dde2ab86d035c2f2d268bf5de32ca0
Majick333 <rmccauleycode@gmail.com>
2020-10-26 22:56:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
- superbowlWin = (record) => {
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+superbowlWin = (record) => {
   let result = record.find( record => record.result === "W" )
-  return result.year	  return !!result ? result.year : undefined
+  return !!result ? result.year : undefined
 }
