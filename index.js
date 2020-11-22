61657401ceab4cf1c5e471dabfa3da67c6e0b676
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-22 16:54:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
-}
 
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-  //...
-]
-
-function superbowlWin(record) {
-  return record.find()
+superbowlWin = record => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
