d22771cd3e390874ed7af9b626079f62b90a817f
RyanBegnaud <Ryanbegnaud13@gmail.com>
2020-12-10 15:46:40 -0800

    Done.

--- index.js
+++ index.js
@@ -6,9 +6,5 @@ function testFunc() {
 
 function superbowlWin(objArray) {
   let win = objArray.find(object => object.result === "W")
-  if(win) {
-    return win.year
-  } else {
-    return undefined
-  }
+  return !!win ? win.year : undefined
 }
