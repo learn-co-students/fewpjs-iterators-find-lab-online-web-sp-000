4ee443e3149480d1a95f603c33afd706db217356
denalibalser <denali.balser167@gmail.com>
2020-12-14 11:25:10 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  let obj = array.find(o => o.result === "W")
+  if(obj) {
+    return obj.year
+  }
+  else {
+    return undefined
+  } 
+}
