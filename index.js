690f9e6ad9c7f15df802a67f5ac16db34961d2e4
ismayton <ian.mayton@gmail.com>
2020-12-30 10:40:48 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winner = array.find( game => game.result === "W" )
+  if (winner) {
+    return winner.year
+  }
+}
