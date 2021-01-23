44b70a4aa582ebd49a231f64e1b3eb722605bb83
Brian Vehige <brianvehige@gmail.com>
2021-01-23 12:25:57 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+  //...
+]
+function superbowlWin(record) {
+  let winnerYear = record.find((y) => y.result === "W")
+  if (winnerYear) {
+    return winnerYear.year
+  }
+  return undefined
+}
+superbowlWin(record)
+
