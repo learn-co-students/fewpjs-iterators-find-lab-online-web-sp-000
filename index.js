bfed2d850aab5d152d4484f26c2319273d9be3a1
seth <seth.j.near@gmail.com>
2021-01-24 22:18:36 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(stats) {
+let rslt;
+stats.find(record => {
+  record.result == "W" ? rslt = record.year : rslt;
+})
+return rslt;
+}
