c450b7ddacca470c210f77caaa125d2ec1e5bec2
learn-co-git <argus.two.2@protonmail.com>
2021-01-25 21:12:24 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let solution = arr.find(ele => ele.result === "W");
+  if (solution == undefined) {
+    return undefined;
+  } else {
+    return solution.year;
+  }
+}
