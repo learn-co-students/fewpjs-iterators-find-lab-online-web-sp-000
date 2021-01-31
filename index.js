5910e97f2c3bdf426ad6a8a48e8b6bbd2659fd0d
AustinRhoads <austinrrhoads@ymail.com>
2021-01-31 13:18:59 -0600

    this is the day our lives will surely change

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let found = array.find((game) => game.result === "W")
+  if (found){return found.year}
+  }
