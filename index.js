5733773fd8e2d16c3d02bb86d7e88809cfb86bf9
Catherine Gloria <catherinegloria@gmail.com>
2021-02-13 07:21:54 -0800

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
-}
-
 function superbowlWin(record) {
   let win = record.find(obj => obj.result === "W");
   return !!win ? win.year : "undefined"
