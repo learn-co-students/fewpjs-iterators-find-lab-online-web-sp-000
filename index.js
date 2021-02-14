b8c619a3611538d2c3f7bd2e113af1f852862fd5
anpham148 <anpham148@gmail.com>
2021-02-14 11:10:28 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  const r = arr.find(s => s.result === "W");
+  if (r) {
+    return r.year
+  } else {
+    return undefined
+  }
+}
