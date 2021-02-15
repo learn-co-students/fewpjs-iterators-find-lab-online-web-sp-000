a01023a44228d72bab0e60b0419083f34805e615
khoinguyenkc <khoinguyenkc@gmail.com>
2021-02-15 12:22:42 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+console.log("hiiii")
+
+
+
+function superbowlWin(arrayOfObjects) {
+  let result = arrayOfObjects.find( function(object) { return object.result === "W"});
+  if (result) {
+    return result.year
+  } else {
+    return result
+  }
+}
