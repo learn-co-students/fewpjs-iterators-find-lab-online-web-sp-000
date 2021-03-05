b2c62ac8c6b02f289a884547a20687d9c9c54362
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 14:43:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+  
 const testVar = {}
 
 function testFunc() {
   return "hi"
+}
+
+const superbowlWin = array => {
+  let result = array.find(element => element.result === "W");
+  return !!result ? result.year : undefined;
 }
