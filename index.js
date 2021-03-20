09f95cfa5339f3e5643a64169bc4efb357148796
Carrie Mathieu <carrieburks@gmail.com>
2021-03-20 12:25:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin (objArray) {
+  // return year when win occured or undefined if no win
+  let winYear = objArray.find(win => win.result === "W")
+  if (winYear) {
+    return winYear.year
+  } else { 
+    undefined
+  }
 }
+
+// function winner(win) {
+//   if (win.result === "W") {
+//    value = win.year
+//   }
+// }
