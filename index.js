a2ac70cb96d5095f581cef437f1ad72b7b2fdc42
Tres Tarwater <trest03@gmail.com>
2021-03-26 19:51:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
-
 function testFunc() {
   return "hi"
+}
+
+function superbowlWin(records) {
+  if (records.find(record => record['result']==="W"))
+    return records.find(record => record['result']==="W")['year']
+  else
+    return undefined
 }
