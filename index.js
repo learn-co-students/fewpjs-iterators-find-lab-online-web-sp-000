8df8e3d3519401a4aca8a4761a842608af428237
jeanettepayne <jeanetterpayne@gmail.com>
2021-04-03 14:43:44 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objArray) {
+  let result = objArray.find(element => element.result === "W")
+  return !!result ? result.year : undefined
+  }
