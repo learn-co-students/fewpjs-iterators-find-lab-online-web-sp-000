f4fbf301bd4f9df7f78bd08432e47c0cba024ff5
AlexJMartinez <alexandr3martin3z@gmail.com>
2021-04-18 13:58:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+
+function superbowlWin(record) {
+  let result = record.find( ({ result }) => result === 'W' ); 
+  return !!result ? result.year : undefined 
 }
+
+
