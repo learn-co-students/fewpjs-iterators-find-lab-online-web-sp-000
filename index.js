72e7206fefc07dd695f1c7c82c5a55ef33575134
Anna Guerrero <acg2176@columbia.edu>
2021-04-23 00:45:57 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(games) {
+	let win = games.find(superbowl => superbowl.result === "W")
+	return win ? win.year : undefined;
+}
