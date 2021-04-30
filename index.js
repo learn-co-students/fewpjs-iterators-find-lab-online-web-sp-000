ed51a9c9a50b58f729fede0aa0d3c120e52b5d7f
Keith Filkins <kafilkins@me.com>
2021-04-30 11:55:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+superbowlWin = (array) => {
+  let result = array.find( array => array.result === "W" )
+  return !!result ? result.year : undefined 
 }
