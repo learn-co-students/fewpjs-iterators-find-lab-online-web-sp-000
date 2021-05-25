3528cbbe2c3e17df4687ff677da720e19f314337
asb1512 <andrew@olympicmedia.com>
2021-05-24 22:31:11 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObjects) {
+  const win = arrayOfObjects.find(o => o.result === "W")
+  if (win != undefined) {
+    return win.year
+  } else {
+    return undefined
+  }
+}
