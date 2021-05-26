a73f5612d5bc07d05f6fd03e80e55b1b8725383e
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-26 14:44:35 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let win = arr.find(e => (e["result"] == "W"))
+  if (win) {
+    return win.year
+  } else {
+    win
+  }
+}
