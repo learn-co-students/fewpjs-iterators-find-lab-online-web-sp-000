5e8f4d123490034e2447e6515d9c96338b8747aa
Benjamin Cheng <bencheng200@gmail.com>
2021-05-30 14:27:42 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let result = arr.find(arr => arr.result === 'W')
+  
+  return !!result ? result.year : undefined
+}
